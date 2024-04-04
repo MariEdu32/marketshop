8e5574076bdecc46261d9c0ecbe2359d08ed0653
MariEdu32 <fernandesmotamariaeduarda35@gmail.com>
2024-04-04 10:51:25 -0300

    criaçao de layout base, menu e alteração de home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,46 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+<x-layout_base>
 
-    <title>MarketShop - Home</title>
-</head>
-<body>
-    
-    <div>
-        Teste de conteúdo!!
+    <x-menu></x-menu>
+
+    <div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_667836-MLA74349586053_022024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_806000-MLA75393863908_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_859602-MLA75368444934_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_922106-MLA75377276862_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a> 
+      <a href="#slide5" class="btn btn-circle">❯</a>
+    </div>
+    <div id="slide5" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_997780-MLA75425140714_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+
+    <div class="bg-blue-200">
+        TESTE
     </div>
 
-</body>
-</html>
+</x-layout_base>
