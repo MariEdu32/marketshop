b3dae40e5076d944848e27bb6f85fdcb02966f90
MariEdu32 <fernandesmotamariaeduarda35@gmail.com>
2024-05-02 10:48:23 -0300

    comando inicia aula, salvar usuario no banco, tabela de produtos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 Route::get('/home', function () {
     return view('welcome');
@@ -14,7 +15,15 @@ Route::view('/cria-conta', 'cria-conta');
 Route::view('testedeconteudo', 'teste');
 
 Route::post('/salva-usuario', function (Request $request) {
-    dd($request);
+    // dd($request);
+
+    $usuario = new User();
+    $usuario->name = $request->nome;
+    $usuario->email = $request->email;
+    $usuario->password = $request->senha;
+    $usuario->save();
+    dd("Salvo com sucesso!💕");
+
 })->name('salva-usuario');
 
 
